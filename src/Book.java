fdf7672c48ae91b329b5d467d709967147698b49
igor <melnik.net@gmail.com>
2025-01-17 17:25:55 +0300

    homework to check

---
+++ src/Book.java
@@ -0,0 +1,33 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "книга: " + "\"" + title + "\" автор: " + author.toString() + " (" + publicationYear + ")";
+   }
+
+}
